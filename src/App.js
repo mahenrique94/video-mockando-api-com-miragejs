f480c9be27d4ad3aab0a279c0a0bfeec8a0483fa
Matheus Castiglioni <matheushcastiglioni@gmail.com>
2021-07-30 09:33:53 -0300

    Enviando para GH

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import axios from 'redaxios'
+
+const http = axios.create({
+  baseURL: 'http://api.matheuscastiglioni.com.br'
+})
 
 function App() {
+  const [tasks, updateTasks] = useState([])
+  const [task, updateTask] = useState('')
+  const [isLoading, updateIsLoading] = useState(false)
+
+  useEffect(() => {
+    updateIsLoading(true)
+    http.get('/api/tasks')
+      .then(response => {
+        updateTasks(response.data.tasks)
+        updateIsLoading(false)
+      })
+  }, [])
+
+  const handleAddTask = event => {
+    event.preventDefault()
+    if (task.trim()) {
+      updateIsLoading(true)
+      http.post('/api/tasks', { task })
+        .then(response => {
+          updateTasks(oldTasks => [...oldTasks, response.data.task])
+          updateTask('')
+          updateIsLoading(false)
+        })
+    }
+  }
+
+  if (isLoading) return <h1>Está carregando...</h1>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Video mockando API</h1>
+      <form onSubmit={handleAddTask}>
+        <input onChange={event => updateTask(event.target.value)} value={task} />
+        <button>Add</button>
+      </form>
+      <ul>
+        {tasks.map(t => <li key={t.id}>{t.name}</li>)}
+      </ul>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Factory, Model, Server } from 'miragejs'
+
+new Server({
+  models: {
+    task: Model,
+  },
+
+  factories: {
+    task: Factory.extend({
+      name(i) {
+        return `Tarefa ${i + 1}`
+      },
+    })
+  },
+
+  seeds(server) {
+    server.create('task', { name: 'Minha tarefa' })
+    server.createList('task', 12)
+  },
+
+  routes() {
+    this.namespace = 'api'
+    this.urlPrefix = 'http://api.matheuscastiglioni.com.br'
+
+    this.get('/tasks', async (schema) => {
+      return {
+        tasks: (await schema.tasks.all()).models,
+      }
+    }, { timing: 3000 })
+
+    this.post('/tasks', async (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+      return {
+        task: (await schema.tasks.create({ name: data.task })).attrs
+      }
+    }, { timing: 5000 })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
